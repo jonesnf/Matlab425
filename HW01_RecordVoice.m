bc4399dcc9a01e078c4d5bf6be24ac38db5d0549
Nate Jones <jonesnf@miamioh.edu>
2017-02-15 11:21:52 -0500

    Finished hw

--- HW01_RecordVoice.m
+++ HW01_RecordVoice.m
@@ -1,25 +1,26 @@
-r = audiorecorder(11025, 16,1);
+r = audiorecorder(11025, 16, 1);
 recordblocking(r, 3);
 disp('Done recording...');
-play(r);
+%play(r);
 x = getaudiodata(r, 'double');
 plot(x);
 
-x2 = x*0.95;
+x2 = x;
+x2 = (x2 ./ max(x2))*0.95;
 figure;
 plot(x2);
 
 r = audioplayer(x2, 11025);
  
 disp('Writing .wav file...');
-filename = 'hw01FirstWave.wav';
 audiowrite('hw01FirstWave.wav', x, 11025);  %Using for comparing 
 audiowrite('hw01SecWave.wav', x2, 11025);
 audiowrite('hw01ThdWave.wav', x2, 22050);
 
-
+z1 = zeros(1, 29);
+z2 = zeros(1, 69);
 a = 1;
-b = [0.7 0.2 0.1];
+b = [0.7 z1 0.2 z2 0.1];
 t = 1:length(x2);
 y = filter(b,a,x2);
 figure;
