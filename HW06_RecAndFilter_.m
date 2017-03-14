bb3e3001bce7dd270927f910954feb10305362cc
Nate Jones <jonesnf@miamioh.edu>
2017-03-14 16:50:31 -0400

    updated hw/shoudl b finished

--- HW06_RecAndFilter_.m
+++ HW06_RecAndFilter_.m
@@ -5,7 +5,6 @@ recordblocking(r, 3);
 disp('Done recording...');
 x = getaudiodata(r, 'double');
     
-    
 
 x2 = x;
 x2 = (x2 ./ max(abs(x2)))*0.95;
@@ -36,9 +35,8 @@ audiowrite('hw06Origin_8000.wav', x2, 8000);
 audiowrite('hw06Noise_8000.wav', y, 8000);
 audiowrite('hw06Filter_8000.wav', z, 8000);
 
-
-%error = zeros(size(x2));
-%error = x2 - z;
+error = x2 - z;
+disp(error);
     
 relErr = zeros(size(x2));
 sum = zeros(size(x2));
@@ -52,46 +50,52 @@ total = 0;
     
 disp('Finished');
 keydown = waitforbuttonpress;
-    
+
+%%%%%%%------Waiting for key press to start part two-------%%%%%%%
+
 disp('Recording voice...');
 r = audiorecorder(44100, 16, 1);
 recordblocking(r, 3);
 disp('Done recording...');
 x = getaudiodata(r, 'double');
 
- x2 = x;
- x2 = (x2 ./ max(abs(x2)))*0.95;
+x2 = x;
+x2 = (x2 ./ max(abs(x2)))*0.95;
 
     
- r = audioplayer(x2, 44100);
- play(r);
+%r = audioplayer(x2, 44100);
+%play(r);
     
 noise = randn(length(x2), 1);
 
 noise = (noise ./ max(abs(noise))) * 0.1;
 y = x2 + noise;
 
-r = audioplayer(y, 44100);
-play(r);
+%r = audioplayer(y, 44100);
+%play(r);
 
 a = 1;
 b = [0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1];
 z = filter(b, a, y);
 
 
-r = audioplayer(y, 44100);
-play(r);
+%r = audioplayer(y, 44100);
+%play(r);
 
 error = x - z;
-    
+disp(error);
+
+audiowrite('hw06Origin_44100.wav', x2, 44100);
+audiowrite('hw06Noise_44100.wav', y, 44100);
+audiowrite('hw06Filter_44100.wav', z, 44100);
+
 relErr = zeros(size(x2));
 sum = zeros(size(x2));
 total = 0;
-    
     for i = 1: numel(x2)
         sum(i) = sqrt(abs(x(i) - z(i)));
         relErr(i) = sum(i) ./ sqrt(abs(x(i)));   
         total  = total + relErr(i);
     end
 
-disp('Finished');
+disp('Finished program');
