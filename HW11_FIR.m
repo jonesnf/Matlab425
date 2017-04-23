87c99596177c8047e5d6bf21e8fed25b24c1f7d4
Nate Jones <jonesnf@miamioh.edu>
2017-04-23 19:54:55 -0400

    Finished HW11

--- HW11_FIR.m
+++ HW11_FIR.m
@@ -4,7 +4,7 @@ Fs = 8000;
 filename = 'test_voice.wav';
 [x, Fs] = audioread(filename);
 
-audiowrite('testhw11_origin.wav', x, Fs);
+audiowrite('HW11_origin.wav', x, Fs);
 
 figure;
 freqz(x, 1, 1024, 8000);
@@ -12,19 +12,15 @@ freqz(x, 1, 1024, 8000);
 
 wc1 = 2*pi*(2027/8000);
 wc2 = 2*pi*(2400/8000);
-N = 55;                              %picking random order
-n = -((N-1)/2):((N-1)/2);            % centering around zero
-n = n+(n==0)*eps;
+N = 55;                              %picking higher order even though more expensive
+n = -((N-1)/2):((N-1)/2);            %centering around zero
+n = n+(n==0)*eps;                    %Avoid division by zero
 [h] = -(sin(n * wc2) - sin(n * wc1))./( n * pi);
 h(28) = 1 + h(28);
 
 w = 1;
 d = h .* w;
 
-
-%[w] = 0.54 + 0.46*cos(2 * pi * n/N);
-%d = h;
-%fvtool(d, 1);
 
 figure;
 freqz(d, 1, 1024, 8000);
@@ -33,7 +29,10 @@ figure;
 [y, f]= filter(d, 1, x);
 freqz(y,1,1024,8000);
 
-audiowrite('testHw11.wav',y,Fs);
+%printing filter coefficients
+disp(h);
+
+audiowrite('HW11_FIR.wav',y,Fs);
 
 
 
